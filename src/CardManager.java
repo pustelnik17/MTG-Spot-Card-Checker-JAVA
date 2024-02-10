41c966bf244e6cb4493438d61d326c8bb8558288
FoxFate <solembum.pospolity@gmail.com>
2024-02-10 17:42:25 +0100

    Add input parsing

--- src/CardManager.java
+++ src/CardManager.java
@@ -1,6 +1,16 @@
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CardManager {
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.print(FileReader.getLines());
+        System.out.print(parse());
+    }
+    private static List<String> parse() throws FileNotFoundException {
+        List<String> result = FileReader.getLines();
+        for(int i = 0; i < result.size(); i++){
+            result.set(i, result.get(i).replaceFirst("[0-9]* ", ""));
+        }
+        return result;
     }
 }
